3d39bc2dd5e08079e794a9daaf2b341b6ec25856
Paniagua96 <paniagua-11@hotmail.com>
2019-05-08 13:37:03 -0500

    Se tiene el movimiento y disparo de la nave

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed=20;
+
+    void Start()
+    {
+        Destroy(gameObject,3);
+    }
+
+    void Update()
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    public Transform Canon;
+    public GameObject bullet;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 DirectionX = Input.GetAxis("Horizontal") * Vector3.right;
+        Vector3 DirectionZ = Input.GetAxis("Vertical") * Vector3.forward;
+
+        Vector3 Direction = DirectionX + DirectionZ;
+        Vector3 VectorVelocity = Direction * speed;
+
+        rb.velocity = VectorVelocity;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Instantiate(bullet, Canon.position, bullet.transform.rotation);
+
+        }
+
+    }
+}
