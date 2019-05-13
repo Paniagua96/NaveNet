2340628a470286c3687e8c201a78419e8c048142
FernanD02 <43126842+FernanD02@users.noreply.github.com>
2019-05-13 13:22:44 -0500

    Primera version del lloby del juego, se requiere cambios en el prefab del jugador. Se cambio la escena de Game.

---
+++ Assets/Lobby/StartShip.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartShip : MonoBehaviour
+{
+    public GameObject ship1;
+    public GameObject ship2;
+    public GameObject ship3;
+    public GameObject finalShip;
+
+    void Start()
+    {
+        if (SingletonShip.ship == 1)
+        {
+            finalShip = Instantiate(ship1, this.transform.position, Quaternion.identity);
+            finalShip.transform.Rotate(-90, 0,0);
+            finalShip.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            Destroy(this);
+        }else if (SingletonShip.ship == 2)
+        {
+            finalShip = Instantiate(ship2, transform.position, Quaternion.identity);
+            finalShip.transform.Rotate(-90, 0, 0);
+            finalShip.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            Destroy(this);
+        }else if (SingletonShip.ship == 3)
+        {
+            finalShip = Instantiate(ship3, transform.position, Quaternion.identity);
+            finalShip.transform.Rotate(-90, 0, 0);
+            finalShip.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            Destroy(this);
+        }
+    }
+}
+
+   
